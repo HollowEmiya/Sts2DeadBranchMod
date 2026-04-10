89faabdb1253712c1f5209320984d18c28350133
Emiya <3033554546@qq.com>
2026-04-10 20:57:44 +0800

    修改DeadBranch下IrconcladCard的错误路径。

--- Scripts/Cards/DRModCard.cs
+++ Scripts/Cards/DRModCard.cs
@@ -2,7 +2,7 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Models;
 public abstract class DRModelIroncladCardModel : CardModel
 {
-    public override string PortraitPath => $"res://DeaBranchMod/images/Cards/IroncladCards/{GetType().Name}.png";
+    public override string PortraitPath => $"res://DeadBranchMod/images/Cards/IroncladCards/{GetType().Name}.png";
 
     public DRModelIroncladCardModel(int energyCost, CardType type,
         CardRarity rarity, TargetType targetType, bool shouldShowInCardLibrary = true) :
